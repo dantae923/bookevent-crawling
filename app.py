d6eb564a9854715beb9b4912b55cd7189ba622bb
김영준 <youngjoonkim94@gmail.com>
2025-02-10 00:10:10 +0900

    대원씨아이추가

--- app.py
+++ app.py
@@ -261,6 +261,58 @@ def crawl_animate_event_details(search_query):
         print(f"[{end_time.strftime('%Y-%m-%d %H:%M:%S')}] 애니메이트 크롤링 종료")
         print(f"실행 시간: {end_time - start_time}")
 
+# 대원씨아이
+
+def crawl_daewon_event_details(search_query):
+    start_time = datetime.now()
+    print(f"[{start_time.strftime('%Y-%m-%d %H:%M:%S')}] 대원씨아이 크롤링 시작")
+    url = "https://dwcishop.co.kr/product/list.html?cate_no=450"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        # print(response.text)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        data = []
+        items = soup.select('div.name a')
+        #print(items)
+        for item in items:
+            link_tag = item.get('href', '#')
+            link = f"https://dwcishop.co.kr/{link_tag}" if link_tag.startswith(
+                "/") else link_tag
+            #print(item)
+            #title_tag = item.select_one('span:last-child')
+            spans = item.find_all("span")
+            title = spans[-1].text.strip() if spans else "제목 없음"
+            #print(title)
+            #title = title_tag.text.strip() if title_tag else "제목 없음"
+            image_tag = item.select_one('img')
+            image = image_tag["src"] if image_tag else "이미지 없음"
+            period_tag = item.select_one('span.date')
+            period = period_tag.text.strip() if period_tag else "기간 정보 없음"
+
+            if title != '제목 없음' and search_query.replace(" ", "") in title.replace(" ", ""):
+                data.append({
+                    'site': '대원씨아이',
+                    'title': title,
+                    'link': link,
+                    'image': image,
+                    'period': period
+                })
+            #print(data)
+        return data
+    except Exception as e:
+        print(f"크롤링 중 오류 발생: {e}")
+        return []
+    finally:
+        end_time = datetime.now()
+        print(f"[{end_time.strftime('%Y-%m-%d %H:%M:%S')}] 대원씨아이 크롤링 종료")
+        print(f"실행 시간: {end_time - start_time}")
+
 
 # 모든 사이트 크롤링
 def crawl_all_events(search_query):
@@ -298,6 +350,8 @@ def home():
             events += crawl_aladin_event_details(search_query)
         if "animate" in selected_sites:
             events += crawl_animate_event_details(search_query)
+        if "daewon" in selected_sites:
+            events += crawl_daewon_event_details(search_query)
 
         return render_template('index.html', events=events, query=search_query)
     return render_template('search.html')
